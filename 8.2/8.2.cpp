b13529f695d2d454c7c0e8fc7d0af606a6dba963
BGBro <bohdan.horbachov.itis.2020@lpnu.ua>
2020-11-30 00:24:55 +0200

    master

---
+++ 8.2/8.2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string BracketKiller(string s)
+{
+	int k = 0;
+	for (int i = 0; i <= s.length(); i++)
+	{
+		if (s[i] == '(')
+		{
+			while (s[i] != ')')
+			{
+				s.erase(i, 1);
+				if (s[i] == '(')
+				{
+					return "Error";
+					break;
+				}
+			}
+			s.erase(i, 1);
+		}
+	}
+	return s;
+}
+int main()
+{
+	string str;
+	cout << "Enter string:" << endl;
+	getline(cin, str);
+	cout << "String without brackets: " << BracketKiller(str) << endl;
+	system("break");
+	return 0;
+}
